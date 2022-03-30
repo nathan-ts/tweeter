2e81263f4406e5038c93cc85e76cc8ad88c4abff
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-30 23:02:12 +0000

    add initial framework for button and nav display when scrolling

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Update character counter on new-tweet 
   $("textarea[name^='text']").on("input", function() {
     // Calculate remaining chars
     const remain = 140 - $(this).val().length;
@@ -11,5 +12,45 @@ $(document).ready(function() {
     // Push value
     $(this).next().children('output').html(remain);
   })
+
+  // $(window).scroll(
+  //   function (event) {
+  //     var scrolled_val = window.scrollY;
+  //     console.log("Scroll:",scrolled_val);
+  //   }
+  // );
+
+  // Observer on header (when it scrolls out of viewport)
+  const pageHeader = document.querySelector('.page-header')
+  const observer = new window.IntersectionObserver(([entry]) => {
+    if (!entry.isIntersecting) {
+      // CODE TO EXECUTE when header scrolls out of view
+      console.log('leave');
+      $('nav').fadeOut('slow');
+      $('.btn-return-top').fadeIn('slow');
+      return
+    }
+    // CODE TO EXECUTE when header enters view
+    console.log('enter');
+    $('nav').slideDown('slow');
+    $('.btn-return-top').hide();
+  }, {
+    root: null,
+    threshold: 0.1, // set offset 0.1 means trigger if atleast 10% of element in viewport
+  })
+  // https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
+
+  // Start Intersection Observer and hide return-top button
+  observer.observe(pageHeader);
+  $('.btn-return-top').hide();
+
+  // Button behaviour for return-to-top button visible when header scrolled out of view
+  $('.btn-return-top').on("click", function() {
+    document.getElementsByTagName('html')[0].scrollIntoView({ behavior: "smooth" })
+    $('.new-tweet').slideDown('fast');
+
+  });
+  // https://stackoverflow.com/questions/1144805/scroll-to-the-top-of-the-page-using-javascript
+
 });
 
