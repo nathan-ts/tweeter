2b693f0973305b4dd74aa0801a61fe1e8d0c6089
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-30 00:04:49 +0000

    update composer to inherit colour:

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $("textarea[name^='text']").on("input", function() {
     // Calculate remaining chars
     const remain = 140 - $(this).val().length;
-    // Set text to red if negative; otherwise white
+    // Set colour to red if negative; otherwise re-inherit parent colour
     if (remain < 0) {
       $(this).next().children('output').css("color", "#ff0000");
     } else {
-      $(this).next().children('output').css("color", "#ffffff");
+      $(this).next().children('output').css("color", $(this).next().css('color'));
     }
     // Push value
     $(this).next().children('output').html(remain);
